9239bfd30d98247dee21cddea8c087203a2f5424
Gokul Vasudeva <gokul.vasda@gmail.com>
2024-01-31 22:13:15 -0700

    Done, TODO: setup Gunicorn

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from pandas import read_csv
+
+model = read_csv("model_res.csv")
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    image_name = request.files['inputFile'].filename.split('.')[0]
+    return image_name + ':' + model.loc[(model['Image'] == image_name)]['Results'].values[0]
+
+
+if __name__ == "__main__":
+    app.run()
