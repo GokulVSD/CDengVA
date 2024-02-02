421b1e185cb7889e19703bfcde62e16adb7db663
Gokul Vasudeva <gokul.vasda@gmail.com>
2024-02-01 18:49:11 -0700

    Make userdata run every time

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum install git
-curl -O https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
-git clone https://github.com/GokulVSD/CDengVA.git
-cd CDengVA/
-git pull
-sudo python3 -m pip install -r requirements.txt
-sudo gunicorn app:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
