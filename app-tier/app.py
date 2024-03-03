0116fe03ced1485033b7718c866febb8f525ca26
Gokul Vasudeva <gokul.vasda@gmail.com>
2024-03-02 22:14:07 -0700

    Start working on app tier for part 2

---
+++ app-tier/app.py
@@ -0,0 +1,64 @@
+import os
+import subprocess
+
+import boto3
+
+sqs = boto3.resource('sqs', region_name='us-east-1')
+s3 = boto3.resource('s3', region_name='us-east-1')
+
+
+def main():
+    req_queue = sqs.get_queue_by_name(QueueName="1229503862-req-queue")
+    resp_queue = sqs.get_queue_by_name(QueueName="1229503862-resp-queue")
+
+    in_bucket = s3.Bucket('1229503862-in-bucket')
+    out_bucket = s3.Bucket('1229503862-out-bucket')
+
+    while True:
+
+        # Read message from request sqs.
+        messages = req_queue.receive_messages(
+            MessageAttributeNames=["All"],
+            MaxNumberOfMessages=1,
+            VisibilityTimeout=30,
+            WaitTimeSeconds=10,
+        )
+
+        if not messages:
+            break
+
+        message = messages[0]
+
+        # Download image, run inference, upload image to s3.
+        s3_image_filename = message.body
+
+        in_bucket.download_file(s3_image_filename, s3_image_filename)
+
+        result = subprocess.run(
+            ['python3', 'face_recognition.py', s3_image_filename],
+            stdout=subprocess.PIPE
+        ).stdout.decode('utf-8')
+
+        image_name = s3_image_filename.split('.')[0]
+
+        with open(image_name, "w") as f:
+            f.write(result)
+
+        out_bucket.upload_file(image_name, image_name)
+
+        # Cleanup.
+        if os.path.exists(image_name):
+            os.remove(image_name)
+
+        if os.path.exists(s3_image_filename):
+            os.remove(s3_image_filename)
+
+        # Send message to response sqs.
+        resp_queue.send_message(MessageBody=image_name)
+
+        # After successfully processing this request, delete the message.
+        req_queue.delete_messages(Entries=[{"Id": 0, "ReceiptHandle": message.receipt_handle}])
+
+
+if __name__ == "__main__":
+    main()
