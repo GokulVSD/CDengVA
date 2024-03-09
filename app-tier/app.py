de6b18f8ce4f1d3f84cb7325502548a1fd2de7db
Gokul Vasudeva <gokul.vasda@gmail.com>
2024-03-09 06:32:46 -0700

    Attempting to fix problems, maybe break needs to be removed

--- app-tier/app.py
+++ app-tier/app.py
@@ -1,38 +1,31 @@
+import random
 import os
 import subprocess
+import time
 
 import boto3
-# from botocore.exceptions import ClientError
 
 sqs = boto3.resource('sqs', region_name='us-east-1')
 s3 = boto3.resource('s3', region_name='us-east-1')
 
-# s3_client = boto3.client('s3')
-
 req_queue = sqs.get_queue_by_name(QueueName="1229503862-req-queue")
 resp_queue = sqs.get_queue_by_name(QueueName="1229503862-resp-queue")
 
-IN_BUCKET_NAME = '1229503862-in-bucket'
-OUT_BUCKET_NAME = '1229503862-out-bucket'
+in_bucket = s3.Bucket('1229503862-in-bucket')
+out_bucket = s3.Bucket('1229503862-out-bucket')
 
-in_bucket = s3.Bucket(IN_BUCKET_NAME)
-out_bucket = s3.Bucket(OUT_BUCKET_NAME)
-
-# def check_key_exists_in_output_bucket(key):
-#     try:
-#         s3_client.head_object(Bucket=OUT_BUCKET_NAME, Key=key)
-#         return True
-#     except ClientError as e:
-#         return False
 
 def main():
 
     while True:
+        # Sleep randomly between 1 and 5 seconds to reduce chance of race conditions,
+        # since SQS standard queue can duplicate messages to different workers.
+        time.sleep(random.uniform(1, 5))
 
         # Read message from request sqs.
         messages = req_queue.receive_messages(
             MaxNumberOfMessages=1,
-            VisibilityTimeout=15,
+            VisibilityTimeout=10,
             WaitTimeSeconds=0,
         )
 
@@ -70,6 +63,7 @@ def main():
 
         # After successfully processing this request, delete the message.
         message.delete()
+        break
 
 
 if __name__ == "__main__":
